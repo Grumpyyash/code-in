1b336f4abdeee15d72d5776dd2d8d1f1b12f08c5
Yash Vardhan <yashvardhan513@gmail.com>
2021-03-14 23:22:40 +0530

    upadte app.js

--- app.js
+++ app.js
@@ -327,12 +327,10 @@ app.post("/foundForm",upload.single("profile"),function(req, res){
 
 app.post("/search", function(req, res){
   const requestedItem = _.lowerCase(req.body.search);
-  let x = 0;
-  let string = "";
-  let count = 0;
+  var x = 0;
   Lost.find({}, function(err, lostItems){
-    lostItems.forEach(function(item){
-      const storedTitle = _.lowerCase(item.category);
+    for(var i=0;i<lostItems.length;i++){
+      var storedTitle = _.lowerCase(lostItems[i].category);
       if (storedTitle === requestedItem) {
         if(storedTitle === "electronics"){
           Electronics.find({}, function(err, electronics){
@@ -340,55 +338,51 @@ app.post("/search", function(req, res){
               name: "ELECTRONICS",
               number: electronics.length
             });
-          })
+          });
+          x=1;
+          break;
         } else if(storedTitle === "vehicles") {
           Vehicle.find({}, function(err, vehicles){
             res.render("categoryDetail", {
               name: "VEHICLES",
               number: vehicles.length
             });
-          })
+          });
+          x=1;
+          break;
         } else if(storedTitle === "wallets") {
           Wallet.find({}, function(err, wallets){
             res.render("categoryDetail", {
               name: "WALLETS",
               number: wallets.length
             });
-          })
+          });
+          x=1;
+          break;
         } else if(storedTitle === "briefcases") {
           Briefcase.find({}, function(err, briefcases){
             res.render("categoryDetail", {
               name: "BRIEFCASES",
               number: briefcases.length
             });
-          })
+          });
+          x=1;
+          break;
         } else if(storedTitle === "cards") {
           Card.find({}, function(err, cards){
             res.render("categoryDetail", {
               name: "CARDS",
               number: cards.length
             });
-          })
-        } else {
-          Other.find({}, function(err, others){
-            res.render("categoryDetail", {
-              name: "OTHER PRODUCTS",
-              number: others.length
-            });
-          })
+          });
+          x=1;
+          break;
         }
-        x=1;
+        
       }
-    });
-    if(x==0) {
-      Other.find({}, function(err, others){
-        res.render("categoryDetail", {
-          name: "OTHER PRODUCTS",
-          number: others.length
-        });
-      });     
     }
   });
+  
 });
 
 
